099b26c24e748183c5dc323d4cb0c1a63fa2f114
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-09-05 23:17:27 -0300

    🩹 fix: remove data files

--- examples/polynomial.py
+++ examples/polynomial.py
@@ -1,21 +1,14 @@
-from typing import List
-
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
 from natural_computing.objective_functions import SquaredError
 from natural_computing.optimization import BareBonesParticleSwarmOptimization
 from natural_computing.plotting import polynomial_plot
 
-
-def read_txt(path: str) -> List[float]:
-    return pd.read_csv(path, header=None).T.values.tolist()[0]
-
-
 if __name__ == '__main__':
     # Example of using optimizers to find polynomial coefficients.
-    x_data = read_txt('resources/x_data.txt')
-    y_data = read_txt('resources/y_data.txt')
+    x_data = np.linspace(-5, 5, 30)
+    y_data = 2 * x_data**2 - 3 * x_data + 1
 
     objective_function = SquaredError(x_data, y_data, mean=True)
     pso = BareBonesParticleSwarmOptimization(
