196a4638d441875a8117853649384c83d401be14
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-09-13 16:41:57 -0300

    ✨ feat: add animation to polynomial plot

--- examples/polynomial.py
+++ examples/polynomial.py
@@ -1,3 +1,6 @@
+from typing import List
+
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -5,16 +8,40 @@ from natural_computing.objective_functions import SquaredError
 from natural_computing.optimization import BareBonesParticleSwarmOptimization
 from natural_computing.plotting import polynomial_plot
 
+SAVE_GIF = False
+
+
+def update(frame):
+    plt.clf()
+    pso._optimization_step(objective_function)
+    polynomial_plot(pso.best_global_position, (-5, 5), 100, x_data, y_data)
+    plt.xlim([-6, 6])
+    plt.ylim([-5, 75])
+
+
 if __name__ == '__main__':
     # Example of using optimizers to find polynomial coefficients.
     x_data = np.linspace(-5, 5, 30)
     y_data = (2 * x_data**2 - 3 * x_data + 1).tolist()
-    x_data = x_data.tolist()
+    x_data: List[float] = x_data.tolist()
 
     objective_function = SquaredError(x_data, y_data, mean=True)
     pso = BareBonesParticleSwarmOptimization(
-        80, 300, [(-5, 5) for _ in range(3)]
+        20, 300, [(-50, 50) for _ in range(3)]
     )
-    pso.optimize(objective_function)
+
+    fig = plt.figure()
+    pso._optimization_step(objective_function)
     polynomial_plot(pso.best_global_position, (-5, 5), 100, x_data, y_data)
-    plt.show()
+    plt.xlim([-6, 6])
+    plt.ylim([-5, 75])
+
+    ani = animation.FuncAnimation(
+        fig, update, frames=60, repeat=True, interval=300
+    )
+
+    if SAVE_GIF:
+        writer = animation.PillowWriter(fps=10)
+        ani.save('scatter.gif', writer=writer)
+    else:
+        plt.show()
