08c6120317841b60ad2ba1c0b17a080ca22e7be5
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-09-28 11:43:11 -0300

    ✨ feat: add softmax function

--- natural_computing/neural_network/activation_functions.py
+++ natural_computing/neural_network/activation_functions.py
@@ -20,7 +20,7 @@ def sigmoid(x: np.array, derivative: bool = False) -> np.array:
     true, pass the value through the derivative of the sigmoid function.
 
     Args:
-        x (np.array): Layer output.
+        x (np.array): Input vector.
         derivative (bool): indicates whether you want to go through the
             derivative (default is False).
 
@@ -31,3 +31,28 @@ def sigmoid(x: np.array, derivative: bool = False) -> np.array:
         return np.exp(-x) / (np.exp(-x) + 1) ** 2
     else:
         return 1 / (1 + np.exp(-x))
+
+
+def softmax(x: np.array, derivative: bool = False) -> np.array:
+    """
+    Passes the vector through the softmax function.
+
+    Obs.: This implementation subtracts max(x) for numerical stability.
+
+    Args:
+        x (np.array): Input vector.
+        derivative (bool): indicates whether you want to go through the
+            derivative (default is False).
+
+    Returns:
+        np.array: Output vector after applying the softmax function.
+    """
+    e_x = np.exp(x - np.max(x))
+    sum_e_x = e_x.sum(axis=0)
+
+    result = e_x / sum_e_x
+
+    if derivative:
+        result *= (1 - result)
+
+    return result
