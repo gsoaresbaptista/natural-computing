✏️ typos: fix softmax neg log likelihood function name

natural_computing/neural_network/loss_functions.py
@@ -62,7 +62,7 @@ def neg_log_likelihood(
     return np.mean(-np.log(values))
 
 
-def softmax_neg_likelihood(
+def softmax_neg_log_likelihood(
     y: np.array,
     y_pred: np.array,
     derivative: bool = False,